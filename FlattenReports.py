fa0c127ed023c4403ff1a13424c18d60b6e31668
drewkerrigan <dkerrigan@basho.com>
2013-08-23 14:20:03 -0400

    fixing rolling average, added start of flattener but needs work, don't use

--- FlattenReports.py
+++ FlattenReports.py
@@ -16,7 +16,7 @@ def build_latencies(stats_arr, filename):
             if len(stats_arr) <= i:
                 stats_arr.append([elapsed, window, n, minimum, mean, median, nine5, nine9, nine9_9, maximum, errors])
             else: 
-                stats_arr[i][0] = (stats_arr[i][0] + float(elapsed))
+                stats_arr[i][0] = int(max(stats_arr[i][0], float(elapsed)))
                 stats_arr[i][1] = (stats_arr[i][1] + window)
                 stats_arr[i][2] = int(stats_arr[i][2] + n)
                 stats_arr[i][3] = int(min(stats_arr[i][3], minimum))
@@ -32,17 +32,32 @@ def build_latencies(stats_arr, filename):
 
     return stats_arr
 
-def process_latencies(stats_arr, num_reports):
-    for i in range(0, len(stats_arr)):
-        stats_arr[i][0] = stats_arr[i][0] / num_reports
-        stats_arr[i][1] = stats_arr[i][1] / num_reports
-        stats_arr[i][4] = stats_arr[i][4] / num_reports
-        stats_arr[i][5] = int(stats_arr[i][5] / num_reports)
-        stats_arr[i][6] = int(stats_arr[i][6] / num_reports)
-        stats_arr[i][7] = int(stats_arr[i][7] / num_reports)
-        stats_arr[i][8] = int(stats_arr[i][8] / num_reports)
+def flatten_latencies(stats_arr, num_reports):
+    flat_arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    return stats_arr
+    for i in range(0, len(stats_arr)):
+        flat_arr[0] = int(max(stats_arr[i][0], flat_arr[0]))
+        flat_arr[1] += stats_arr[i][1]
+        flat_arr[2] += stats_arr[i][2]
+        flat_arr[3] = int(min(stats_arr[i][3], flat_arr[3]))
+        flat_arr[4] += stats_arr[i][4]
+        flat_arr[5] += stats_arr[i][5]
+        flat_arr[6] += stats_arr[i][6]
+        flat_arr[7] += stats_arr[i][7]
+        flat_arr[8] += stats_arr[i][8]
+        flat_arr[9] = int(max(stats_arr[i][9], flat_arr[9]))
+        flat_arr[10] += stats_arr[i][10]
+
+    print flat_arr
+
+    flat_arr[1] = flat_arr[1] / num_reports
+    flat_arr[4] = flat_arr[4] / len(stats_arr) / num_reports
+    flat_arr[5] = flat_arr[5] / len(stats_arr) / num_reports
+    flat_arr[6] = flat_arr[6] / len(stats_arr) / num_reports
+    flat_arr[7] = flat_arr[7] / len(stats_arr) / num_reports
+    flat_arr[8] = flat_arr[8] / len(stats_arr) / num_reports
+
+    return flat_arr
 
 def build_summary(stats_arr, filename):
     i = 0
@@ -58,7 +73,7 @@ def build_summary(stats_arr, filename):
             
             if len(stats_arr) <= i:
                 stats_arr.append([elapsed, window, total, successful, failed])
-            else: 
+            else:
                 stats_arr[i][0] = stats_arr[i][0] + float(elapsed)
                 stats_arr[i][1] = stats_arr[i][1] + window
                 stats_arr[i][2] = int(stats_arr[i][2] + total)
@@ -66,27 +81,35 @@ def build_summary(stats_arr, filename):
                 stats_arr[i][4] = int(stats_arr[i][4] + failed)
             
             i += 1
-            
+
     return stats_arr
 
-def process_summary(stats_arr, num_reports):
+def flatten_summary(stats_arr, num_reports):
+    flat_arr = [0, 0, 0, 0, 0]
+
     for i in range(0, len(stats_arr)):
-        stats_arr[i][0] = stats_arr[i][0] / num_reports
-        stats_arr[i][1] = stats_arr[i][1] / num_reports
+        flat_arr[0] += stats_arr[i][0]
+        flat_arr[1] += stats_arr[i][1]
+        flat_arr[2] += stats_arr[i][2]
+        flat_arr[3] += stats_arr[i][3]
+        flat_arr[4] += stats_arr[i][4]
+
+    flat_arr[0] = flat_arr[0] / len(stats_arr) / num_reports
+    flat_arr[1] = flat_arr[1] / len(stats_arr) / num_reports
             
-    return stats_arr
+    return flat_arr
 
 parser = argparse.ArgumentParser(description='Combine basho_bench results into a single directory')
 parser.add_argument('-i', '--input', dest='inputs', nargs='+',
                    help='list of basho bench result directories')
 
-parser.add_argument('-o', '--output', dest='output',
-                   help='result directory')
+# parser.add_argument('-o', '--output', dest='output',
+#                    help='result directory')
 
 args = parser.parse_args()
 
-if not os.path.exists(args.output):
-    os.makedirs(args.output)
+# if not os.path.exists(args.output):
+#     os.makedirs(args.output)
 
 latency_dict = {}
 
@@ -100,19 +123,15 @@ for directory in args.inputs:
 
 #Write Latencies
 for latency_name in latency_dict:
-    f = open(args.output + "/" + latency_name, 'w+')
-    f.write("elapsed, window, n, min, mean, median, 95th, 99th, 99_9th, max, errors\n")
-    for row in process_latencies(latency_dict[latency_name], len(args.inputs)):
-        f.write(','.join(map(str,row)) + '\n')
-    f.close
+    print latency_name
+    print "elapsed, window, n, min, mean, median, 95th, 99th, 99_9th, max, errors"
+    print (','.join(map(str,flatten_latencies(latency_dict[latency_name], len(args.inputs)))))
 
 # #Write Summary
 summary_arr = []
 for directory in args.inputs:
     summary_arr = build_summary(summary_arr, directory + '/summary.csv')
     
-f = open(args.output + '/summary.csv', 'w')
-f.write("elapsed, window, total, successful, failed\n")
-for row in process_summary(summary_arr, len(args.inputs)):
-    f.write(','.join(map(str,row)) + '\n')
-f.close
+print "\nsummary.csv"
+print "elapsed, window, total, successful, failed"
+print (','.join(map(str,flatten_summary(summary_arr, len(args.inputs)))))
